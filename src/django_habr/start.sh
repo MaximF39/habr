6cef7573b84732d0934ee849825b956d61ce5e7c
maxf39 <maks.fedotow14@mail.ru>
2022-09-23 18:30:26 +0500

    Add: nginx and gunicorn

--- src/django_habr/start.sh
+++ src/django_habr/start.sh
@@ -2,6 +2,6 @@
 
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-python manage.py createsuperuser --noinput
+python manage.py createsuperuser --no-input
 gunicorn django_habr.wsgi:application -c gunicorn.conf.py
 
